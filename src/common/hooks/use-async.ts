69be585419fa36982b4992f3d746226a0186093e
Karine Souza <kszmr@hotmail.com>
2020-12-22 22:36:02 -0300

    hook useAsync

---
+++ src/common/hooks/use-async.ts
@@ -0,0 +1,31 @@
+import {useState} from 'react';
+
+interface AsyncReturnType <T> {
+    isLoading: boolean;
+    error: boolean;
+    result: T | undefined;
+}
+
+export const useAsync = <T> (asyncFn: Promise<T>):AsyncReturnType<T> => {
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(false);
+    const [result, setResult] = useState<T> ();
+    
+    useEffect(() => {
+        const fn = async() => {
+            setIsLoading(true);
+            setError(false);
+            try {
+                const result = await asyncFn;
+                setIsLoading(false);
+                setResult(result);
+            } catch (error) {
+                setIsLoading(false);
+                setError(true);
+            }
+        }
+        fn();
+    }, [setIsLoading, setResult, setError, asyncFn])
+
+    return {isLoading, error, result};
+}
